ac5245de30b07d547cc089488983b00f91ea9f53
WolverinDEV <Myhu2106>
2019-03-04 19:49:04 +0100

    Added country flags

--- shared/js/load.ts
+++ shared/js/load.ts
@@ -631,6 +631,7 @@ const loader_style = {
             "css/static/ts/tab.css",
             "css/static/ts/chat.css",
             "css/static/ts/icons.css",
+            "css/static/ts/country.css",
             "css/static/general.css",
             "css/static/modals.css",
             "css/static/modal-bookmarks.css",
