efb4bad182f4471ea8d5ef234f84c88a62079277
WolverinDEV <git@mcgalaxy.de>
2019-04-04 18:15:28 +0200

    improved literal handling on type declaration

--- tools/dtsgen/declarator.ts
+++ tools/dtsgen/declarator.ts
@@ -371,7 +371,24 @@ generators[SyntaxKind.VariableStatement] = (settings, stack, node: ts.VariableSt
 };
 
 generators[SyntaxKind.TypeAliasDeclaration] = (settings, stack, node: ts.TypeAliasDeclaration) => {
-    return node;
+    let type = node.type;
+    if(type.kind == SyntaxKind.UnionType) {
+        const union_members = [];
+        const union = <ts.UnionTypeNode>node.type;
+
+        for(const element of union.types as any as any[]) {
+            if(element.kind === SyntaxKind.LiteralType && element.literal && element.literal.text) {
+                union_members.push(ts.createStringLiteral(element.literal.text));
+            }
+            else
+                union_members.push(element);
+            console.log(SyntaxKind[element.kind]);
+        }
+
+        type = ts.createUnionTypeNode(union_members);
+    }
+
+    return ts.createTypeAliasDeclaration(node.decorators, node.modifiers, node.name, node.typeParameters, type);
 };
 
 generators[SyntaxKind.EnumMember] = (settings, stack, node: ts.EnumMember) => {
---
+++ tools/dtsgen/test/test_04.ts
@@ -0,0 +1,5 @@
+type T = "a" | "b" | "c";
+
+function _t(parm: T) {}
+
+function __t() {}
