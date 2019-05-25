11bb4ab2185e5abfb4994f461c3b7da4a6faef0c
WolverinDEV <git@mcgalaxy.de>
2019-05-25 20:59:41 +0200

    file cleanup

--- client/declarations/exports.d.ts
+++
@@ -1,8 +0,0 @@
-
-/* File: client/js/teaforo.ts */
-declare namespace forum {
-    export function register_callback(callback: () => any);
-    export function open();
-    export function logout();
-    export function sync_main();
-}
