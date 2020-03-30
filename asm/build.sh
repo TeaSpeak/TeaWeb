8f80ba42e8e67336d8e55add14e7e6b462fa35b1
WolverinDEV <git@teaspeak.de>
2020-03-30 18:16:03 +0200

    Updated the opus build and enforced as emscripten web build

---
+++ asm/build.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+[[ ! -d libraries/opus/out/ ]] && { echo "Missing opus build. Please build it before!"; exit 1; }
+[[ ! -f libraries/opus/out/lib/libopus.a ]] && { echo "Missing opus static library. Please unsure your opus build was successfull."; exit 1; }
+
+[[ -d build_ ]] && {
+  rm -r build_ || { echo "failed to remove old build directory"; exit 1; }
+}
+mkdir build_ || exit 1
+cd build_ || exit 1
+
+emcmake cmake .. || {
+  echo "Failed to execute cmake"
+  exit 1
+}
+
+emmake make || {
+  echo "Failed to build file"
+  exit 1
+}
