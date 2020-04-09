c22dfc4fe05b86695d9617da246a6468d3b5ba36
WolverinDEV <git@teaspeak.de>
2020-04-09 18:13:18 +0200

    Fixed the deploy file grep part

--- scripts/travis_deploy_dev_server.sh
+++ scripts/travis_deploy_dev_server.sh
@@ -14,9 +14,9 @@ chmod 600 /tmp/sftp_key
 }
 cd "$(dirname "$0")/.." || { echo "Failed to enter base dir"; exit 1; }
 
-file=$(find "$PACKAGES_DIRECTORY" -maxdepth 1 -name "*.zip" -print)
+file=$(find "$PACKAGES_DIRECTORY" -maxdepth 1 -name "TeaWeb-Release*.zip" -print)
 [[ $(echo "$file" | wc -l) -ne 1 ]] && {
-    echo "Invalid release file count (Expected 1 but got $(echo "$file" | wc -l))"
+    echo "Invalid release file count (Expected 1 but got $(echo "$file" | wc -l)): ${file[*]}"
     exit 1
 }
 [[ ! -e "$file" ]] && {
