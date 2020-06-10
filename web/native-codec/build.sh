c9474ad0118f742a68a823a9d34a28a65e1f8233
WolverinDEV <git@teaspeak.de>
2020-06-10 18:48:48 +0200

    Added a work around for the, sometimes not working emcmake command

--- web/native-codec/build.sh
+++ web/native-codec/build.sh
@@ -4,15 +4,19 @@ cd "$(dirname "$0")" || { echo "Failed to enter base dir"; exit 1; }
 [[ -d build_ ]] && {
   rm -r build_ || { echo "failed to remove old build directory"; exit 1; }
 }
+
 mkdir build_ || exit 1
 cd build_ || exit 1
 
 emcmake cmake .. || {
-  echo "Failed to execute cmake"
-  exit 1
+    echo "emcmake cmake failed for the first time, trying it again" #IDKW but sometimes it does not work the first try
+    emcmake cmake .. || {
+        echo "Failed to execute cmake"
+        exit 1
+    }
 }
 
 emmake make -j"$(nproc --all)" || {
-  echo "Failed to build file"
-  exit 1
+    echo "Failed to build file"
+    exit 1
 }
