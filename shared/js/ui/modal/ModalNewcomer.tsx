b8fbd58276268dd0c9f6d65e711f58b5e576440f
WolverinDEV <git@teaspeak.de>
2020-08-19 22:40:40 +0200

    Fixed the microphone selection for the newcomer modal

--- shared/js/ui/modal/ModalNewcomer.tsx
+++ shared/js/ui/modal/ModalNewcomer.tsx
@@ -5,6 +5,10 @@ import { modal as emodal } from "tc-shared/events";
 import {modal_settings} from "tc-shared/ui/modal/ModalSettings";
 import {profiles} from "tc-shared/profiles/ConnectionProfile";
 import {spawnYesNo} from "tc-shared/ui/modal/ModalYesNo";
+import {initialize_audio_microphone_controller, MicrophoneSettingsEvents} from "tc-shared/ui/modal/settings/Microphone";
+import {MicrophoneSettings} from "tc-shared/ui/modal/settings/MicrophoneRenderer";
+import * as React from "react";
+import * as ReactDOM from "react-dom";
 
 const next_step: {[key: string]:string} = {
     "welcome": "microphone",
@@ -69,7 +73,7 @@ function initializeBasicFunctionality(tag: JQuery, event_registry: Registry<emod
         tag_body.find(".step").addClass("hidden");
         container_header.find(".step").addClass("hidden");
 
-         tag_body.find(".step.step-" + event.step).removeClass("hidden");
+        tag_body.find(".step.step-" + event.step).removeClass("hidden");
         container_header.find(".step.step-" + event.step).removeClass("hidden");
     });
 
@@ -79,7 +83,7 @@ function initializeBasicFunctionality(tag: JQuery, event_registry: Registry<emod
         const button_last_step = buttons.find(".button-last-step");
         const button_next_step = buttons.find(".button-next-step");
 
-        button_last_step.on('click', event => {
+        button_last_step.on('click', () => {
             if(last_step[current_step])
                 event_registry.fire("show_step", { step: last_step[current_step] as any });
             else
@@ -100,8 +104,8 @@ function initializeBasicFunctionality(tag: JQuery, event_registry: Registry<emod
             button_last_step.text(last_step[current_step] ? tr("Last step") : tr("Skip guide"));
         });
 
-        event_registry.on("show_step", event => button_next_step.prop("disabled", true));
-        event_registry.on("show_step", event => button_last_step.prop("disabled", true));
+        event_registry.on("show_step", () => button_next_step.prop("disabled", true));
+        event_registry.on("show_step", () => button_last_step.prop("disabled", true));
 
         event_registry.on("step-status", event => button_next_step.prop("disabled", !event.next_button));
         event_registry.on("step-status", event => button_last_step.prop("disabled", !event.previous_button));
@@ -292,145 +296,28 @@ function initializeStepIdentity(tag: JQuery, event_registry: Registry<emodal.new
 }
 
 function initializeStepMicrophone(tag: JQuery, event_registry: Registry<emodal.newcomer>, modal: Modal) {
-    const microphone_events = new Registry<emodal.settings.microphone>();
-    //microphone_events.enable_debug("settings-microphone");
-    //modal_settings.initialize_audio_microphone_controller(microphone_events);
-    //modal_settings.initialize_audio_microphone_view(tag, microphone_events);
-    modal.close_listener.push(() => microphone_events.fire_async("deinitialize"));
+    let helpStep = 0;
 
-    let help_animation_done = false;
-    const update_step_status = () => event_registry.fire_async("step-status", { next_button: help_animation_done, previous_button: help_animation_done });
-    event_registry.on("show_step", e => {
-        if(e.step !== "microphone") return;
+    const settingEvents = new Registry<MicrophoneSettingsEvents>();
+    settingEvents.on("query_help", () => settingEvents.fire_async("notify_highlight", { field: helpStep <= 2 ? ("hs-" + helpStep) as any : undefined }));
+    settingEvents.on("action_help_click", () => {
+        helpStep++;
+        settingEvents.fire("query_help");
 
-        update_step_status();
+        event_registry.fire_async("step-status", { next_button: helpStep > 2, previous_button: helpStep > 2 })
     });
 
-    /* the help sequence */
-    {
-        const container = tag.find(".container-settings-audio-microphone");
-        const container_help_text = tag.find(".container-help-text");
+    initialize_audio_microphone_controller(settingEvents);
+    ReactDOM.render(<MicrophoneSettings events={settingEvents} />, tag[0]);
 
-        const container_profile_list = tag.find(".highlight-microphone-list");
-        const container_profile_settings = tag.find(".highlight-microphone-settings");
+    modal.close_listener.push(() => {
+        settingEvents.fire("notify_destroy");
+        ReactDOM.unmountComponentAtNode(tag[0]);
+    });
 
-        let is_first_show = true;
-        event_registry.on("show_step", event => {
-            if(!is_first_show || event.step !== "microphone") return;
-            is_first_show = false;
 
-            container.addClass("help-shown");
-            const text = tr( /* @tr-ignore */
-                "Firstly we need to setup a microphone.\n" +
-                "Let me guide you thru the basic UI elements.\n" +
-                "\n" +
-                "To continue click anywhere on the screen."
-            );
-            set_help_text(text);
-            $("body").one('mousedown', event => show_microphone_list_help());
-        });
-
-        const set_help_text = text => {
-            container_help_text.empty();
-            text.split("\n").forEach(e => container_help_text.append(e == "" ? $.spawn("br") : $.spawn("a").text(e)));
-        };
-
-        const show_microphone_list_help = () => {
-            container.find(".highlighted").removeClass("highlighted");
-            container_profile_list.addClass("highlighted");
-
-            const update_position = () => {
-                const font_size = parseFloat(getComputedStyle(container_help_text[0]).fontSize);
-
-                const offset = container_profile_list.offset();
-                const abs = container.offset();
-
-                container_help_text.css({
-                    top: offset.top - abs.top,
-                    left: ((offset.left - abs.left) + container_profile_list.outerWidth() + font_size) + "px",
-                    right: "1em",
-                    bottom: "1em"
-                });
-            };
-            update_position();
-            container_help_text.off('resize').on('resize', update_position);
-
-            const text = tr( /* @tr-ignore */
-                "All your available microphones are listed within this box.\n" +
-                "\n" +
-                "The currently selected microphone\n" +
-                "is marked with a green checkmark. To change the selected microphone\n" +
-                "just click on the new one.\n" +
-                "\n" +
-                "To continue click anywhere on the screen."
-            );
-            set_help_text(text);
-            $("body").one('mousedown', event => show_microphone_settings_help());
-        };
-
-        const show_microphone_settings_help = () => {
-            container.find(".highlighted").removeClass("highlighted");
-            container_profile_settings.addClass("highlighted");
-
-            const update_position = () => {
-                const font_size = parseFloat(getComputedStyle(container_help_text[0]).fontSize);
-                const container_settings_offset = container_profile_settings.offset();
-                const right = container_profile_settings.outerWidth() + font_size * 2;
-                container_help_text.css({
-                    top: container_settings_offset.top - container.offset().top,
-                    left: "1em",
-                    right: right + "px",
-                    bottom: "1em"
-                });
-            };
-
-            container_help_text.empty();
-            container_help_text.append($.spawn("div").addClass("help-microphone-settings").append(
-                $.spawn("a").text(tr("On the right side you'll find all microphone settings.")),
-                $.spawn("br"),
-                $.spawn("a").text("TeaSpeak has three voice activity detection types:"),
-                $.spawn("ol").append(
-                    $.spawn("li").addClass("vad-type").append(
-                        $.spawn("a").addClass("title").text(tr("Push to Talk")),
-                        $.spawn("a").addClass("description").html(tr( /* @tr-ignore */
-                            "To transmit audio data you'll have to<br>" +
-                            "press a key. The key could be selected " +
-                            "via the button right to the radio button."
-                        ))
-                    ),
-                    $.spawn("li").addClass("vad-type").append(
-                        $.spawn("a").addClass("title").text(tr("Voice activity detection")),
-                        $.spawn("a").addClass("description").html(tr( /* @tr-ignore */
-                            "In this mode, TeaSpeak will continuously analyze your microphone input. " +
-                            "If the audio level is grater than a certain threshold, " +
-                            "the audio will be transmitted. " +
-                            "The threshold is changeable via the \"Sensitivity Settings\" slider."
-                        ))
-                    ),
-                    $.spawn("li").addClass("vad-type").append(
-                        $.spawn("a").addClass("title").html(tr("Always active")),
-                        $.spawn("a").addClass("description").text(tr( /* @tr-ignore */
-                            "Continuously transmit any audio data.\n"
-                        ))
-                    )
-                ),
-                $.spawn("br"),
-                $.spawn("a").text(tr("Now you're ready to configure your microphone. Just click anywhere on the screen."))
-            ));
-            update_position();
-            container_help_text.off('resize').on('resize', update_position);
-
-            $("body").one('mousedown', event => hide_help());
-        };
-
-        const hide_help = () => {
-            container.find(".highlighted").removeClass("highlighted");
-            container.addClass("hide-help");
-            setTimeout(() => container.removeClass("help-shown"), 1000);
-            container_help_text.off('resize');
-
-            help_animation_done = true;
-            update_step_status();
-        };
-    }
+    event_registry.on("show_step", event => {
+        if(event.step !== "microphone") return;
+        event_registry.fire_async("step-status", { next_button: helpStep > 2, previous_button: helpStep > 2 });
+    });
 }
