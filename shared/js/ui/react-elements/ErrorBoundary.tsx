a5afa5cce39b182edb4b553b647c36e16055a447
WolverinDEV <git@did.science>
2020-12-18 19:25:27 +0100

    Added some error boundaries to keep the ui functional even if elements may throw and error for a connection instance

--- shared/js/ui/react-elements/ErrorBoundary.tsx
+++ shared/js/ui/react-elements/ErrorBoundary.tsx
@@ -1,20 +1,31 @@
 import * as React from "react";
 
+const cssStyle = require("./ErrorBoundary.scss");
+
 interface ErrorBoundaryState {
     errorOccurred: boolean
 }
 
 export class ErrorBoundary extends React.Component<{}, ErrorBoundaryState> {
+    constructor(props) {
+        super(props);
+
+        this.state = { errorOccurred: false };
+    }
     render() {
         if(this.state.errorOccurred) {
-
+            return (
+                <div className={cssStyle.container}>
+                    <div className={cssStyle.text}>A rendering error has occurred</div>
+                </div>
+            );
         } else {
             return this.props.children;
         }
     }
 
     componentDidCatch(error: Error, errorInfo: React.ErrorInfo) {
-        console.error("Did catch: %o - %o", error, errorInfo);
+        /* TODO: Some kind of logging? */
     }
 
     static getDerivedStateFromError() : Partial<ErrorBoundaryState> {
