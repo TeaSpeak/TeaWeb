65406447f58fba3d69b2aa86d65a5725d749abdb
WolverinDEV <git@did.science>
2020-12-21 20:24:00 +0100

    Fixed too early initialize

--- shared/js/text/bbcode/renderer.ts
+++ shared/js/text/bbcode/renderer.ts
@@ -1,16 +1,29 @@
+import * as loader from "tc-loader";
+import {Stage} from "tc-loader";
+
+import * as React from "react";
+import {Context} from "react";
+
 import TextRenderer from "vendor/xbbcode/renderer/text";
 import ReactRenderer from "vendor/xbbcode/renderer/react";
 import HTMLRenderer from "vendor/xbbcode/renderer/html";
-
-export const BBCodeHandlerContext = React.createContext<string>(undefined);
-
-export const rendererText = new TextRenderer();
-export const rendererReact = new ReactRenderer();
-export const rendererHTML = new HTMLRenderer(rendererReact);
 
 import "./emoji";
 import "./highlight";
 import "./youtube";
 import "./url";
 import "./image";
-import * as React from "react";
+
+export let BBCodeHandlerContext: Context<string>;
+
+export const rendererText = new TextRenderer();
+export const rendererReact = new ReactRenderer();
+export const rendererHTML = new HTMLRenderer(rendererReact);
+
+loader.register_task(Stage.JAVASCRIPT_INITIALIZING, {
+    name: "BBCode handler context",
+    function: async () => {
+        BBCodeHandlerContext = React.createContext<string>(undefined);
+    },
+    priority: 80
+})
